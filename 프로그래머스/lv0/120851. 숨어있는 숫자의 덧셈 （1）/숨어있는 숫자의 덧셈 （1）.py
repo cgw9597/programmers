d682bd0f4153e91f0904c02f2de3a81a38bfe747
cgw9597 <cgw9597@naver.com>
2023-06-24 10:43:12 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,7 +1,17 @@
-import re
+# import re
+# def solution(my_string):
+#     answer = 0
+#     my_str = re.findall('\d',my_string)
+#     a = map(int,my_str)
+#     answer = sum(a)
+#     return answer
+
 def solution(my_string):
     answer = 0
-    my_str = re.findall('\d',my_string)
-    a = map(int,my_str)
-    answer = sum(a)
+    for i in my_string:
+        try:
+            answer += int(i)
+        except:
+            pass
+
     return answer
