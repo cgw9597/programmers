231172ba6b93bd86be0a1bc10256d65b9d0948f0
cgw9597 <cgw9597@naver.com>
2023-07-03 17:55:43 +0900

    [unrated] Title: 0 떼기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
@@ -0,0 +1,8 @@
+def solution(n_str):
+    answer = n_str.lstrip('0')
+    return answer
+
+# def solution(n_str):
+#     for i in range(len(n_str)):
+#         if n_str[i] != '0':
+#             return n_str[i:]
