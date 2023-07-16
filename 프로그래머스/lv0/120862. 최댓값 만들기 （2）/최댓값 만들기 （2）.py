be542d57f1a4ee11f41eabede23cafbd7d219eeb
cgw9597 <cgw9597@naver.com>
2023-07-16 14:18:36 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = 0
+    numbers.sort()
+    if numbers[0]*numbers[1]>numbers[-1]*numbers[-2]:
+        answer = numbers[0]*numbers[1]
+    else:
+        answer = numbers[-1]*numbers[-2]
+    return answer
+
+# def solution(numbers):
+#     numbers.sort()
+#     return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2])
