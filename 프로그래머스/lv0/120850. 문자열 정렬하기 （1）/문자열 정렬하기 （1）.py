e864280ffab5e6a5192a6fefbad4efc28f6e7e0d
cgw9597 <cgw9597@naver.com>
2023-07-17 15:17:56 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    string_list = list(my_string)
+    for i in range(len(string_list)):
+        if string_list[i].isdigit() == True:
+            answer.append(int(string_list[i]))
+    answer.sort()
+    return answer
