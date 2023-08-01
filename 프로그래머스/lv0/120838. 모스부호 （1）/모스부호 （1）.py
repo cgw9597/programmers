18ed7c0b1c2e5cfd607887cfc3b2664d545a8f0b
cgw9597 <cgw9597@naver.com>
2023-08-01 21:26:23 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,14 @@
+def solution(letter):
+    answer = ''
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    # s_letter = letter.split()
+    # a_letter = [morse[case] for case in s_letter]
+    # answer = ''.join(a_letter)
+    
+    return ''.join([morse[i] for i in letter.split()])
