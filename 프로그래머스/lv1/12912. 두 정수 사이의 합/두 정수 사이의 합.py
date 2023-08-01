a108008bb96e40974a492f1172d5841a4680c352
cgw9597 <cgw9597@naver.com>
2023-08-01 20:29:20 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,3 +1,9 @@
+# def solution(a, b):
+#     if a>b:
+#         a,b = b,a
+#     answer = sum(range(a,b+1))
+#     return answer
+
 def solution(a, b):
     answer = (abs(a-b)+1)*(a+b)//2
     return answer
