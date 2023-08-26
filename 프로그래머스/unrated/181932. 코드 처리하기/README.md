859cee043ab45ec83282c549cc034b068a87ffa8
cgw9597 <cgw9597@naver.com>
2023-08-26 16:29:31 +0900

    [unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/README.md
+++ 프로그래머스/unrated/181932. 코드 처리하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 코드 처리하기 - 181932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -1,28 +1,27 @@
 def solution(code):
     answer = ''
     mode = 0
-    for idx,value in enumerate(code):
-        if mode ==0 and value == '1':
-            mode = 1
-        elif mode == 1 and value == '1':
-            mode = 0
-        elif mode == 0 and value != '1' and idx%2 ==0:
-            answer+=value
-        elif mode ==1 and value !='1' and idx%2 !=0:
-            answer+=value
-    return answer if len(answer) !=0 else "EMPTY" 
+    for i in range(len(code)):
+        if code[i] == '1':
+            mode ^= 1
+        else:
+            if i % 2 == mode:
+                answer += code[i]
+
+    return answer if answer else 'EMPTY'
 
 # def solution(code):
 #     answer = ''
-#     mode=0
-#     for i in range(len(code)):
-#         if i%2==0 and mode==0:
-#             answer+=code[i]
-#         elif code[i]=="1" and mode==0:
-#             mode=1
-#         elif code[i]=="1" and mode==1:
-#             mode=0
-#         elif i%2!=0 and mode==1:
-#             answer+=code[i]
-#     return answer if answer!="" else "EMPTY"
+#     mode = 0
+#     for idx,value in enumerate(code):
+#         if mode ==0 and value == '1':
+#             mode = 1
+#         elif mode == 1 and value == '1':
+#             mode = 0
+#         elif mode == 0 and value != '1' and idx%2 ==0:
+#             answer+=value
+#         elif mode ==1 and value !='1' and idx%2 !=0:
+#             answer+=value
+#     return answer if len(answer) !=0 else "EMPTY"
+
 
