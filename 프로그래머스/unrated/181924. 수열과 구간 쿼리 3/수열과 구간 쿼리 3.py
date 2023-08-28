c8ea34c856b3fa1a3ad5d1a2ffa4fa07544f82bd
cgw9597 <cgw9597@naver.com>
2023-08-28 20:50:51 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,8 +1,4 @@
 def solution(arr, queries):
-    answer = []
-    x=[]
     for a, b in queries:
-        x = arr[a]
-        arr[a] = arr[b]
-        arr[b] = x
+        arr[a], arr[b] = arr[b], arr[a]
     return arr
