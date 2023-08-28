b0849c2cec541689de9692f02c19162e2ec36279
cgw9597 <cgw9597@naver.com>
2023-08-28 15:09:20 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/README.md
+++ 프로그래머스/lv0/120848. 팩토리얼/README.md
@@ -1,10 +1,10 @@
 # [level 0] 팩토리얼 - 120848 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 87.24 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -1,8 +1,17 @@
 def solution(n):
     import math
-    answer = []
-    for i in range(2000):
-        if math.factorial(i)<=n:
-            answer.append(i)
+    num = 10
+    while n < math.factorial(num):
+        num -= 1
+    return num
+
+
+
+# def solution(n):
+#     import math
+#     answer = []
+#     for i in range(2000):
+#         if math.factorial(i)<=n:
+#             answer.append(i)
     
-    return max(answer)
+#     return max(answer)
