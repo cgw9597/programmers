0f57decc6a1d37759db8889c1a75e56a245ba1cd
cgw9597 <cgw9597@naver.com>
2023-08-28 16:48:29 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,8 +1,14 @@
 def solution(my_string, m, c):
-    answer = []
-    li = []
-    for i in range(len(my_string)//m):
-        li.append(my_string[i*m:(i+1)*m])
-    for j in range(len(li)):
-        answer.append(li[j][c-1])
-    return ''.join(answer)
+    return my_string[c-1::m]
+
+
+
+
+# def solution(my_string, m, c):
+#     answer = []
+#     li = []
+#     for i in range(len(my_string)//m):
+#         li.append(my_string[i*m:(i+1)*m])
+#     for j in range(len(li)):
+#         answer.append(li[j][c-1])
+#     return ''.join(answer)
