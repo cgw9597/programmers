efecaace9585a60f091f3185ad903e182caa0ecb
cgw9597 <cgw9597@naver.com>
2023-08-28 16:01:01 +0900

    [unrated] Title: 세로 읽기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -0,0 +1,8 @@
+def solution(my_string, m, c):
+    answer = []
+    li = []
+    for i in range(len(my_string)//m):
+        li.append(my_string[i*m:(i+1)*m])
+    for j in range(len(li)):
+        answer.append(li[j][c-1])
+    return ''.join(answer)
