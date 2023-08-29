7df257826d7a495193318e6b40063d036e196e9e
cgw9597 <cgw9597@naver.com>
2023-08-29 11:20:56 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,19 @@
 def solution(absolutes, signs):
     answer = 0
-    for i in range(len(absolutes)):
-        if signs[i] ==True:
-            answer+=absolutes[i]
+    for ab,si in zip(absolutes, signs):
+        if si:
+            answer+=ab
         else:
-            answer-=absolutes[i]
+            answer-=ab
     return answer
+
+
+
+# def solution(absolutes, signs):
+#     answer = 0
+#     for i in range(len(absolutes)):
+#         if signs[i] ==True:
+#             answer+=absolutes[i]
+#         else:
+#             answer-=absolutes[i]
+#     return answer
