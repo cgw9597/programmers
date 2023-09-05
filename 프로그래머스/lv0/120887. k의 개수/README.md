9d3fe50bb47ee92bc542687f03a2f188112d5bbf
cgw9597 <cgw9597@naver.com>
2023-09-05 11:49:33 +0900

    [level 0] Title: k의 개수, Time: 9.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/README.md
+++ 프로그래머스/lv0/120887. k의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] k의 개수 - 120887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120887?language=python3) 
 
 ### 성능 요약
 
-메모리: 12.5 MB, 시간: 5.89 ms
+메모리: 10.3 MB, 시간: 9.49 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,6 +1,12 @@
+# def solution(i, j, k):
+#     answer = []
+#     for num in range(i,j+1):
+#         answer.append(num)
+#     result = ''.join(map(str,answer))
+#     return result.count(str(k))
+
 def solution(i, j, k):
-    answer = []
-    for num in range(i,j+1):
-        answer.append(num)
-    result = ''.join(map(str,answer))
-    return result.count(str(k))
+    answer = 0
+    for n in range(i, j + 1):
+        answer += str(n).count(str(k))
+    return answer
