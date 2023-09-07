39c7a00411353ab1f6ba046018758c6d8dbec283
cgw9597 <cgw9597@naver.com>
2023-09-07 17:27:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,6 +1,9 @@
+# def solution(numbers):
+#     answer = 0
+#     for i in range(10):
+#         if i not in numbers:
+#             answer+=i
+#     return answer
+
 def solution(numbers):
-    answer = 0
-    for i in range(10):
-        if i not in numbers:
-            answer+=i
-    return answer
+    return sum(i for i in range(10) if i not in numbers )
