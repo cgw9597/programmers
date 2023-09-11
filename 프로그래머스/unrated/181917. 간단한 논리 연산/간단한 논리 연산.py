f3b11a1fe238f918b93c70647103bc93e943dc08
cgw9597 <cgw9597@naver.com>
2023-09-11 14:57:04 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,7 +1,2 @@
 def solution(x1, x2, x3, x4):
-    answer = True
-    if (x1|x2) ==True and (x3|x4) == True:
-        answer = True
-    else:
-        answer = False
-    return answer
+    return (x1|x2)&(x3|x4)
