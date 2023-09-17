4410aebdcd5f8dfc6a9d804e3c2bb529d154130d
cgw9597 <cgw9597@naver.com>
2023-09-17 16:35:17 +0900

    [unrated] Title: 배열 만들기 2, Time: 43.84 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,8 @@
+def solution(l, r):
+    answer = []
+    for i in range(l,r+1):
+        if all(j in ['0', '5'] for j in str(i)):
+            answer.append(i)
+    if len(answer) == 0:
+        answer.append(-1)
+    return answer
