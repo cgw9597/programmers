457543682e0e66894fe3a7b805d537ddfaa4ecb0
cgw9597 <cgw9597@naver.com>
2023-09-18 09:56:34 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,7 +1,9 @@
+# def solution(n):
+#     result =  ""
+#     for i in range(n):
+#         li = ["수","박"] * n
+#         result += li[i]
+#     return result
+
 def solution(n):
-    result =  ""
-    for i in range(n):
-        li = ["수","박"] * n
-        result += li[i]
-    
-    return result
+    return "수박" * (n//2) + "수" * (n%2)
