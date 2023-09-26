a5613a011139470b3baee4a38a1b7d0e4deb4f21
cgw9597 <cgw9597@naver.com>
2023-09-26 12:34:06 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,16 +1,25 @@
+# def solution(left, right):
+#     result = 0
+#     my=list(range(left,right+1))
+#     li=[]
+#     for i in range(left, right+1):
+#         answer = 0
+#         for j in range(1,i+1):
+#             if i%j==0:
+#                 answer+=1
+#         li.append(answer)
+#     for idx,num in enumerate(li):
+#         if num%2==0:
+#             result+=my[idx]
+#         else:
+#             result-=my[idx]
+#     return result
 def solution(left, right):
-    result = 0
-    my=list(range(left,right+1))
-    li=[]
+    answer = 0
+    from math import sqrt
     for i in range(left, right+1):
-        answer = 0
-        for j in range(1,i+1):
-            if i%j==0:
-                answer+=1
-        li.append(answer)
-    for idx,num in enumerate(li):
-        if num%2==0:
-            result+=my[idx]
+        if int(sqrt(i)) == sqrt(i):  # 제곱근이 정수면 약수개수 홀수다
+            answer-=i
         else:
-            result-=my[idx]
-    return result
+            answer+=i
+    return answer
