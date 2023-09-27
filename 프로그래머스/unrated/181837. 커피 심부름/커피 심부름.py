b5d402f1afedfe926e3500faee1fd73733b692cf
cgw9597 <cgw9597@naver.com>
2023-09-27 22:19:30 +0900

    [unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,9 @@
+def solution(order):
+    answer = 0
+    for i in range(len(order)):
+        if order[i] in ("icecafelatte", "cafelatteice",
+                        "hotcafelatte", "cafelattehot","cafelatte"):
+            answer+=5000
+        else:
+            answer+=4500
+    return answer
