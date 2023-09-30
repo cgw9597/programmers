26bb54af36f0d58639bd1f198c1ce350b1ea81d8
cgw9597 <cgw9597@naver.com>
2023-09-30 16:35:17 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/README.md
+++ 프로그래머스/lv0/120843. 공 던지기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 공 던지기 - 120843 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843?language=python3) 
 
 ### 성능 요약
 
-메모리: 295 MB, 시간: 303.64 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,7 +1,9 @@
+# def solution(numbers, k):
+#     answer = 0
+#     numbers=numbers*k
+#     li=[]
+#     for i in range(k):
+#         li+=numbers[::2]
+#     return li[k-1]
 def solution(numbers, k):
-    answer = 0
-    numbers=numbers*k
-    li=[]
-    for i in range(k):
-        li+=numbers[::2]
-    return li[k-1]
+    return numbers[2 * (k - 1) % len(numbers)]
