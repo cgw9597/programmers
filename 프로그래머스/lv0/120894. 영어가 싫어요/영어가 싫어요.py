34546f0f607ce3ab75bb0692cc55403002efefa9
cgw9597 <cgw9597@naver.com>
2023-10-03 20:47:10 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,3 +1,6 @@
 def solution(numbers):
-    answer = numbers.replace("zero","0").replace("one","1").replace("two","2").replace("three","3").replace("four","4").replace("five","5").replace("six","6").replace("seven","7").replace("eight","8").replace("nine","9")
-    return int(answer)
+    li = ["zero", "one", "two", "three", "four", "five",
+          "six", "seven", "eight", "nine"]
+    for num, eng in enumerate(li):
+        numbers = numbers.replace(eng, str(num))
+    return int(numbers)
