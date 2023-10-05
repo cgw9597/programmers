424ab41412b0c08b56b9d6b75d6aead28cff46dc
cgw9597 <cgw9597@naver.com>
2023-10-05 11:13:18 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,3 +1,8 @@
 def solution(balls, share):
     from math import factorial as fac
     return fac(balls)/(fac(balls-share)*fac(share))
+                                            
+                                            
+# import math
+# def solution(balls, share):
+#     return math.comb(balls, share)                                
