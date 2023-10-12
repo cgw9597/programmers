74522d2133b9cc942c5a1ff55348264744688175
cgw9597 <cgw9597@naver.com>
2023-10-12 14:10:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,17 +1,6 @@
 def solution(s):
-    answer = ''
-    li = list(s.lower())
-    for i in range(len(li)):
-        if li[i].isalpha() and len(answer)  == 0:
-            answer+=li[i].upper()
-        elif li[i].isdigit():
-            answer+=li[i]
-        elif li[i-1].isdigit():
-            answer+=li[i]
-        elif li[i-1].isalpha():
-            answer+=li[i]
-        elif li[i]==" ":
-            answer+=li[i]
-        elif li[i-1]==" ":
-            answer+=li[i].upper()
-    return answer
+    test_list = s.split(" ")
+    result=[]
+    for i in test_list :
+        result.append(i.capitalize())
+    return ' '.join(result)
